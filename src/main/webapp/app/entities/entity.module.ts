b2dd58892ba401c89ac49a676eeb920b35ab7731
Jérémy Sanchez <jeremy.3y@gmail.com>
2019-09-13 17:08:36 +0200

    Entities links fixed
    Panel deleted inside routes

--- src/main/webapp/app/entities/entity.module.ts
+++ src/main/webapp/app/entities/entity.module.ts
@@ -1,7 +1,7 @@
 import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { RouterModule } from '@angular/router';
 
-const PATH = 'admin/panel';
+const PATH = 'admin';
 
 @NgModule({
   imports: [
