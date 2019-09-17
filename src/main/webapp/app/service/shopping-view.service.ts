a8778c04eee78461ab6d804de08b852ae5e176c5
Syl-47 <sylvestre.wampach@gmail.com>
2019-09-17 12:00:36 +0200

    charte graphique wip

--- src/main/webapp/app/service/shopping-view.service.ts
+++ src/main/webapp/app/service/shopping-view.service.ts
@@ -12,14 +12,12 @@ export class ShoppingService {
     //.style.width = '500px';
     document.getElementById('mainPage').classList.add('pushMainpage');
     document.getElementById('mainPage').style.marginRight = '500px';
-    document.getElementById('openSideBarShop').style.display = 'none';
   }
 
   closeNav() {
     document.getElementById('mySidenav').classList.remove('showShopping');
     document.getElementById('mainPage').classList.remove('pushMainpage');
     //document.getElementById('mySidenav').style.width = '0';
-    document.getElementById('openSideBarShop').style.display = 'block';
     document.getElementById('mainPage').style.marginRight = '0';
   }
 }
