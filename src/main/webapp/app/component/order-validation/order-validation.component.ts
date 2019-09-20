e16e69a45c5d2b7dd58ad02c1b1a966dd8c86469
Syl-47 <sylvestre.wampach@gmail.com>
2019-09-20 11:28:09 +0200

    card checkout wip

--- src/main/webapp/app/component/order-validation/order-validation.component.ts
+++ src/main/webapp/app/component/order-validation/order-validation.component.ts
@@ -6,7 +6,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./order-validation.component.scss']
 })
 export class OrderValidationComponent implements OnInit {
-  constructor() {}
+  currentPage: number;
+  constructor() {
+    this.currentPage = 1;
+  }
+
+  nextPage() {
+    this.currentPage = this.currentPage + 1;
+    this.checkbounderise();
+  }
+
+  PrevPage() {
+    this.currentPage = this.currentPage - 1;
+    this.checkbounderise();
+  }
+
+  checkbounderise() {
+    if (this.currentPage < 1) {
+      this.currentPage = 1;
+    }
+
+    if (this.currentPage > 3) {
+      this.currentPage = 3;
+    }
+  }
 
   ngOnInit() {}
 }
